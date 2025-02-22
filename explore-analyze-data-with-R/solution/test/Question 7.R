f926b23eee3f5a3d68cc1d163f8a16bec7ccc7e0
Ham-ctrl <54822006+Ham-ctrl@users.noreply.github.com>
2025-02-22 10:06:43 +0000

    Update Question 7.R
    Copy code for Question 7 branch

--- explore-analyze-data-with-R/solution/test/Question 7.R
+++ explore-analyze-data-with-R/solution/test/Question 7.R
@@ -1,1 +1,19 @@
-
+test = list(
+  name = "Question",
+  cases = list(
+    ottr::TestCase$new(
+      hidden = FALSE,
+      name = NA,
+     points = 1.0,
+     success_message = "Well done! You have successfully filtered the data to include observations whose Departure Delay falls within the 1st and 90th quantiles.",
+     failure_message = "Almost there! Ensure you have filtered the **df_flights** data to only include rows where the entry for **DepDelay** column is greater than **depdelay_01pcntile** and the **depdelay_90pcntile**",
+      code {
+        ## TEST ##
+        test_that('there are no outliers', {
+          expect_equal(sum(df_flights$DepDelay < depdelay_01pcntile), 0)
+          expect_equal(sum(df_flights$DepDelay > depdelay_09pcntile), 0)
+        })
+      }
+    )
+  )
+)
